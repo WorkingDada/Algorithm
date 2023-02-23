5f558fb7434482c5b543d042cd06b0c8dbdca6e4
Coding Wojak <6430222521@student.chula.ac.th>
2023-02-24 02:15:20 +0700

    updated

--- ex02e3_bs.cpp
+++ ex02e3_bs.cpp
@@ -2,24 +2,25 @@
 #include <vector>
 using namespace std;
 
-int bsearch(int k,vector<int> v){
-  if (k == v[v.size()-1]) return v.size()-1;
-  if (k == v[0]) return 0;
-  for (int i = v.size()-1 ; i >= 0 ; i--){
-    if (k == v[i]) return i;
-  }
-  return -1;
+int bsearch(int k,vector<int> &v,int start,int stop){
+  if (start == stop) return k == v[start] ? start : -1;
+  int m = (start + stop) >> 1;
+  if (v[m] >= k) return bsearch(k,v,start,m);
+  else return bsearch(k,v,m+1,stop);
 }
 
 int main(){
   int n,m,i;
   vector<int> v;
+  cin >> n >> m;
   while (n--){
     cin >> i;
     v.push_back(i);
   }
   while (m--){
     cin >> i;
-    cout << bsearch(i,v) << "\n";
+    if (i > v[v.size()-1]){ cout << v[v.size()-1] << "\n"; }
+    else if (i < v[0]) { cout << "-1" << "\n"; }
+    else { cout << bsearch(i,v,0,v.size()-1) << "\n"; }
   }
 }
---
+++ trial.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int bsearch(int k,vector<int> &v,int start,int stop){
+    if (k >= v[stop]) return stop;
+    if (stop - start <= 1) {
+        return k >= v[stop] ? stop : k >= v[start] ? start : -1;
+    }
+    //cout << v[start] << " " << v[stop] << "\n";
+    int m = (start + stop) >> 1;
+    if (k >= v[m]) bsearch(k,v,m,stop);
+    else bsearch(k,v,start,m);
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m,i;
+    vector<int> v;
+    cin >> n >> m;
+    while (n--){
+        cin >> i;
+        v.push_back(i);
+    }
+    while (m--){
+        cin >> i;
+        if (i > v[v.size()-1]) cout << v.size()-1 << "\n";
+        else if (i < v[0]) cout << "-1\n";
+        else cout << bsearch(i,v,0,v.size()-1) << "\n";
+    }
+}

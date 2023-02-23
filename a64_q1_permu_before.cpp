61e7fcaa148a922c3e5ca22bacb57c678e6672b5
Coding Wojak <6430222521@student.chula.ac.th>
2023-02-23 14:54:48 +0700

    updated

--- a64_q1_permu_before.cpp
+++ a64_q1_permu_before.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
+
 using namespace std;
 
-void perm_kn(int n,vector<int> &sol,int len,vector<bool> &used,int k) {
-    if (len < k) {
-        for (int i = 1;i<=n;i++) {
-            if (used[i] == false) {
-                used[i] = true;
-                sol[len] = i;
-                perm_kn(n,sol,len+1,used,k);
-                used[i] = false;
-            }
+void perm(int n,vector<int> &sol,int len,int k){
+    if (len < k){
+        for (int i = 0 ; i < n ; i++){
+            sol[len] = i;
+            perm(n,sol,len+1,k);
         }
     } else {
-        for (auto &x : sol) cout << x;
-        cout << endl;
+        for (auto &x : sol){
+            cout << x;
+        }
+        cout << "\n";
     }
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    perm_kn(3)
+    int n,m,t1,t2;
+    cin >> n >> m;
+    vector<int> sol(n);
+    map<int,int> mp1;
+    while (m--){
+        cin >> t1 >> t2;
+        mp1[t2] = t1;
+    }
+    perm(n,sol,0,n);
+    for (auto &x : mp1){
+        cout << x.first << " ";
+    }
 }
